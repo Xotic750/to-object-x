cffd9dd7db8d1f9b362b820ef1882318bcc2436d
Xotic750 <xotic750@gmail.com>
2016-01-20 13:22:53 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,89 @@
+/**
+ * @file
+ * <a href="https://travis-ci.org/Xotic750/to-object-x"
+ * title="Travis status">
+ * <img
+ * src="https://travis-ci.org/Xotic750/to-object-x.svg?branch=master"
+ * alt="Travis status" height="18">
+ * </a>
+ * <a href="https://david-dm.org/Xotic750/to-object-x"
+ * title="Dependency status">
+ * <img src="https://david-dm.org/Xotic750/to-object-x.svg"
+ * alt="Dependency status" height="18"/>
+ * </a>
+ * <a
+ * href="https://david-dm.org/Xotic750/to-object-x#info=devDependencies"
+ * title="devDependency status">
+ * <img
+ * src="https://david-dm.org/Xotic750/to-object-x/dev-status.svg"
+ * alt="devDependency status" height="18"/>
+ * </a>
+ * <a
+ * href="https://badge.fury.io/js/to-object-x"
+ * title="npm version">
+ * <img src="https://badge.fury.io/js/to-object-x.svg"
+ * alt="npm version" height="18">
+ * </a>
+ *
+ * ToObject module.
+ *
+ * <h2>ECMAScript compatibility shims for legacy JavaScript engines</h2>
+ * `es5-shim.js` monkey-patches a JavaScript context to contain all EcmaScript 5
+ * methods that can be faithfully emulated with a legacy JavaScript engine.
+ *
+ * `es5-sham.js` monkey-patches other ES5 methods as closely as possible.
+ * For these methods, as closely as possible to ES5 is not very close.
+ * Many of these shams are intended only to allow code to be written to ES5
+ * without causing run-time errors in older engines. In many cases,
+ * this means that these shams cause many ES5 methods to silently fail.
+ * Decide carefully whether this is what you want. Note: es5-sham.js requires
+ * es5-shim.js to be able to work properly.
+ *
+ * `json3.js` monkey-patches the EcmaScript 5 JSON implimentation faithfully.
+ *
+ * `es6.shim.js` provides compatibility shims so that legacy JavaScript engines
+ * behave as closely as possible to ECMAScript 6 (Harmony).
+ *
+ * @version 1.0.0
+ * @author Xotic750 <Xotic750@gmail.com>
+ * @copyright  Xotic750
+ * @license {@link <https://opensource.org/licenses/MIT> MIT}
+ * @module to-object-x
+ */
+
+/*jslint maxlen:80, es6:false, white:true */
+
+/*jshint bitwise:true, camelcase:true, curly:true, eqeqeq:true, forin:true,
+  freeze:true, futurehostile:true, latedef:true, newcap:true, nocomma:true,
+  nonbsp:true, singleGroups:true, strict:true, undef:true, unused:true,
+  es3:true, esnext:false, plusplus:true, maxparams:1, maxdepth:1,
+  maxstatements:3, maxcomplexity:1 */
+
+/*global module */
+
+;(function () {
+  'use strict';
+
+  var $Object = Object;
+  var $requireObjectCoercible = require('require-object-coercible-x');
+
+  /**
+   * The abstract operation ToObject converts argument to a value of
+   * type Object.
+   *
+   * @param {*} value The `value` to convert.
+   * @throws {TypeError} If `value` is a `null` or `undefined`.
+   * @return {string} The `value` converted to an object.
+   * @example
+   * var ToObject = require('to-object-x');
+   *
+   * ToObject(); // TypeError
+   * ToObject(null); // TypeError
+   * ToObject('abc'); // Object('abc')
+   * ToObject(true); // Object(true)
+   * ToObject(Symbol('foo')); // Object(Symbol('foo'))
+   */
+  module.exports = function ToObject(value) {
+    return $Object($requireObjectCoercible(value));
+  };
+}());
---
+++ tests/spec/test.js
@@ -0,0 +1,88 @@
+/*jslint maxlen:80, es6:true, white:true */
+
+/*jshint bitwise:true, camelcase:true, curly:true, eqeqeq:true, forin:true,
+  freeze:true, futurehostile:true, latedef:true, newcap:true, nocomma:true,
+  nonbsp:true, singleGroups:true, strict:true, undef:true, unused:true,
+  es3:false, esnext:true, plusplus:true, maxparams:1, maxdepth:2,
+  maxstatements:12, maxcomplexity:4 */
+
+/*global JSON:true, expect, module, require, describe, xit, it, returnExports */
+
+(function () {
+  'use strict';
+
+  var hasSymbol = typeof Symbol === 'function' &&
+    typeof Symbol() === 'symbol',
+    ifSymbolIt = hasSymbol ? it : xit,
+    $toObject;
+  if (typeof module === 'object' && module.exports) {
+    require('es5-shim');
+    require('es5-shim/es5-sham');
+    if (typeof JSON === 'undefined') {
+      JSON = {};
+    }
+    require('json3').runInContext(null, JSON);
+    require('es6-shim');
+    $toObject = require('../../index.js');
+  } else {
+    $toObject = returnExports;
+  }
+
+  describe('Basic tests', function () {
+    it('should throw TypeError', function () {
+      expect(function () {
+        $toObject();
+      }).toThrow();
+
+      expect(function () {
+        $toObject(undefined);
+      }).toThrow();
+
+      expect(function () {
+        $toObject(null);
+      }).toThrow();
+    });
+
+    it('should be correct type in each case', function () {
+      expect(typeof $toObject(1)).toBe('object');
+      expect(typeof $toObject(true)).toBe('object');
+      expect(typeof $toObject('')).toBe('object');
+      expect(typeof $toObject([])).toBe('object');
+      expect(typeof $toObject({})).toBe('object');
+      expect(typeof $toObject(Object('a'))).toBe('object');
+      expect(typeof $toObject(function () {})).toBe('function');
+      expect(typeof $toObject(new Date())).toBe('object');
+    });
+
+    it('should have correct values', function () {
+      var str = $toObject('foo');
+      expect(typeof str).toBe('object');
+      expect(str.length).toBe(3);
+      expect(Object.prototype.toString.call(str)).toBe('[object String]');
+      expect(str.toString()).toBe('foo');
+      expect(str.charAt(0)).toBe('f');
+      expect(str.charAt(1)).toBe('o');
+      expect(str.charAt(2)).toBe('o');
+    });
+
+    it('should be same object', function () {
+      var testObject = [];
+      expect($toObject(testObject)).toBe(testObject);
+      testObject = {};
+      expect($toObject(testObject)).toBe(testObject);
+      testObject = function () {};
+      expect($toObject(testObject)).toBe(testObject);
+      testObject = Object('test');
+      expect($toObject(testObject)).toBe(testObject);
+      testObject = Object(true);
+      expect($toObject(testObject)).toBe(testObject);
+      testObject = Object(10);
+      expect($toObject(testObject)).toBe(testObject);
+    });
+
+    ifSymbolIt('should return Symbol', function () {
+      var sym = Symbol('foo');
+      expect($toObject(sym)).toEqual(Object(sym));
+    });
+  });
+}());
