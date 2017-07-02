22c88b438f76ce6a2126b29d68fd6c41dba28ffc
Xotic750 <xotic750@gmail.com>
2017-07-02 16:59:42 +0200

    Update boilerplate and dependecies

--- index.js
+++ index.js
@@ -31,40 +31,33 @@
  *
  * @see {@link http://www.ecma-international.org/ecma-262/6.0/#sec-toobject|7.1.13 ToObject ( argument )}
  *
- * @version 1.2.1
+ * @version 1.3.0
  * @author Xotic750 <Xotic750@gmail.com>
  * @copyright  Xotic750
  * @license {@link <https://opensource.org/licenses/MIT> MIT}
  * @module to-object-x
  */
 
-/* eslint strict: 1, id-length: 1 */
+'use strict';
 
-/* global module */
+var $requireObjectCoercible = require('require-object-coercible-x');
 
-;(function () { // eslint-disable-line no-extra-semi
-
-  'use strict';
-
-  var $requireObjectCoercible = require('require-object-coercible-x');
-
-  /**
-   * The abstract operation ToObject converts argument to a value of
-   * type Object.
-   *
-   * @param {*} value The `value` to convert.
-   * @throws {TypeError} If `value` is a `null` or `undefined`.
-   * @return {!Object} The `value` converted to an object.
-   * @example
-   * var ToObject = require('to-object-x');
-   *
-   * ToObject(); // TypeError
-   * ToObject(null); // TypeError
-   * ToObject('abc'); // Object('abc')
-   * ToObject(true); // Object(true)
-   * ToObject(Symbol('foo')); // Object(Symbol('foo'))
-   */
-  module.exports = function ToObject(value) {
-    return Object($requireObjectCoercible(value));
-  };
-}());
+/**
+ * The abstract operation ToObject converts argument to a value of
+ * type Object.
+ *
+ * @param {*} value - The `value` to convert.
+ * @throws {TypeError} If `value` is a `null` or `undefined`.
+ * @returns {!Object} The `value` converted to an object.
+ * @example
+ * var ToObject = require('to-object-x');
+ *
+ * ToObject(); // TypeError
+ * ToObject(null); // TypeError
+ * ToObject('abc'); // Object('abc')
+ * ToObject(true); // Object(true)
+ * ToObject(Symbol('foo')); // Object(Symbol('foo'))
+ */
+module.exports = function ToObject(value) {
+  return Object($requireObjectCoercible(value));
+};
