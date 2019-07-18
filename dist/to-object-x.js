edc96a972eee0319334c37bb6df5c5a3858ae19b
Graham Fairweather <xotic750@gmail.com>
2019-07-18 20:41:16 +0200

    :bookmark: v2.0.10
    Auto build

--- dist/to-object-x.js
+++ dist/to-object-x.js
@@ -2,13 +2,13 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2015-2017",
-  "date": "2019-07-18T16:22:41.922Z",
+  "date": "2019-07-18T18:41:11.309Z",
   "describe": "",
   "description": "ES6-compliant shim for ToObject.",
   "file": "to-object-x.js",
   "hash": "14399cb7257ca40d6a47",
   "license": "MIT",
-  "version": "2.0.9"
+  "version": "2.0.10"
 }
 */
 (function webpackUniversalModuleDefinition(root, factory) {
