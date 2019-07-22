4063b4209f2e2251f800ac58448c985e6da847ab
Graham Fairweather <xotic750@gmail.com>
2019-07-22 14:35:25 +0200

    :bookmark: v2.0.13
    Dependency updates

--- dist/to-object-x.js
+++ dist/to-object-x.js
@@ -2,13 +2,13 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2015-2017",
-  "date": "2019-07-21T22:37:39.232Z",
+  "date": "2019-07-22T12:35:20.117Z",
   "describe": "",
   "description": "ES6-compliant shim for ToObject.",
   "file": "to-object-x.js",
   "hash": "14399cb7257ca40d6a47",
   "license": "MIT",
-  "version": "2.0.12"
+  "version": "2.0.13"
 }
 */
 (function webpackUniversalModuleDefinition(root, factory) {
