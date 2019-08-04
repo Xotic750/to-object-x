ea85b6039a57bda516b9761d3f8e996988d36f46
Graham Fairweather <xotic750@gmail.com>
2019-08-04 21:34:51 +0200

    Auto run

--- dist/to-object-x.js
+++ dist/to-object-x.js
@@ -2,7 +2,7 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2015-2017",
-  "date": "2019-08-04T14:58:31.277Z",
+  "date": "2019-08-04T19:34:46.651Z",
   "describe": "",
   "description": "ES6-compliant shim for ToObject.",
   "file": "to-object-x.js",
